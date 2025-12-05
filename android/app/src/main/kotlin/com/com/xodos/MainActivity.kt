de29cf084101218bc07109ab93532c636b9630c2
xodiosx <mohammedaleiwi@gmail.com>
2025-12-05 16:29:31 +0300

    1.0.3

--- android/app/src/main/kotlin/com/com/xodos/MainActivity.kt
+++ android/app/src/main/kotlin/com/com/xodos/MainActivity.kt
@@ -16,9 +16,9 @@ class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "android").setMethodCallHandler {
-            // 注册通道并设置方法调用处理器
+            // 
             call, result ->
-            // 判断方法名
+            // 
             when (call.method) {
                 "launchSignal9Page" -> {
                     startActivity(Intent(this, Signal9Activity::class.java))
@@ -28,7 +28,7 @@ class MainActivity: FlutterActivity() {
                     result.success(getApplicationInfo().nativeLibraryDir)
                 }
                 else -> {
-                    // 不支持的方法名
+                    // 
                     result.notImplemented()
                 }
             }
