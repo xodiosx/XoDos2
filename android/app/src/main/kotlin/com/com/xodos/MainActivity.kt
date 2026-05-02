30e238d5ee892348157a4a0e657976b418527bca
xodiosx <mohammedaleiwi@gmail.com>
2026-05-02 18:49:00 +0300

    update x11

--- android/app/src/main/kotlin/com/com/xodos/MainActivity.kt
+++ android/app/src/main/kotlin/com/com/xodos/MainActivity.kt
@@ -2,7 +2,7 @@ package com.com.xodos
 
 import android.util.Log
 import android.content.Intent
-import android.os.Bundle
+import android.os.Bundle // Required import
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -10,12 +10,16 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
 
+    // Add this block to force software rendering
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("enable-software-rendering", true)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "android").setMethodCallHandler {
-            // 
             call, result ->
-            // 
             when (call.method) {
                 "launchSignal9Page" -> {
                     startActivity(Intent(this, Signal9Activity::class.java))
@@ -25,11 +29,9 @@ class MainActivity: FlutterActivity() {
                     result.success(getApplicationInfo().nativeLibraryDir)
                 }
                 else -> {
-                    // 
                     result.notImplemented()
                 }
             }
         }
     }
-
 }
